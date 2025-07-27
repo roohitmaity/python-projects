2f1b15c6032aa402adcbbb9619d8d9b1f6e754a4
Rohit Maity <ethicalhackernii@gmail.com>
2025-07-27 22:20:44 +0530

    updated

--- Qoute Generator/quote_generator_using_file.py
+++ Qoute Generator/quote_generator_using_file.py
@@ -1,7 +1,7 @@
 import random
 import json
 
-with open(r"C:\Users\ethic\Desktop\python projects\Qoute Generator\quotes.json","r") as file:
+with open("quotes.json","r") as file:
     quotes=json.load(file)
 
 def display():
@@ -13,20 +13,20 @@ print("Press (q) to start")
 while True:
     print("Press (y) for again\nPress (e) for exit")
 
-    user_input = input().lower()
+    user_input = input()
     if user_input == 'q':
-        # Show the first quote when 'q' is pressed
+        
         display()
         
     elif user_input == 'y':
-        # Show another quote when 'y' is pressed
+       
         display()
 
     elif user_input == 'e':
-        # Exit the program when 'e' is pressed
+        
         print("Goodbye!")
         break
 
     else:
-        # Handle invalid input
+        
         print("Only (q), (y), and (e) are valid options. Please try again.")
---
+++ Qoute Generator/readme.md
@@ -0,0 +1,6 @@
+# ReadMe First
+### This programme randomly generate quotes with user interactions Stored locally.
+
+I have used `random` and `json` module to randomly choose and handling json file format.
+
+Developed By [Linkedin](https://www.linkedin.com/in/rohitmaity/)
